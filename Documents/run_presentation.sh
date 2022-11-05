75d1a7675ab92ee8ff1e9e672c7a684670124fd4
Ze-Veiga98 <81825844+Ze-Veiga98@users.noreply.github.com>
2022-11-05 17:10:17 +0000

    Add files via upload

---
+++ Documents/run_presentation.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# 
+
+# duration 
+# start-time
+# end-time
+# enduserslide
+# lastminutes
+# hidenotes
+# overridenote
+
+
+pdfpc --duration=20 Solar_Electric_Rickshaw_MscThesis_JV.pdf
